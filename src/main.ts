4f95c38bb2b7f48326694100da7fe51c14ba78b0
vince0656 <vince0656@googlemail.com>
2019-10-03 12:49:15 +0100

    Stripped back vue app and added vue dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import Vue from 'vue';
+import VueForm from 'vue-form';
+import VueMoment from 'vue-moment';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueMoment);
+Vue.use(VueForm, {
+  inputClasses: {
+    valid: 'form-control-success',
+    invalid: 'form-control-danger',
+  },
+});
 
 new Vue({
   router,
