b600e90cb9e529c91c15a16bcfc60c7ea2f952bd
James Morgan <james.morgan.e@googlemail.com>
2020-07-13 11:29:33 +0100

    Adding deployment for TL admin

--- firebase_deploy-prod.sh
+++ firebase_deploy-prod.sh
@@ -4,4 +4,6 @@ npm run build;
 
 firebase use tokenlandia-admin;
 
-firebase deploy;
+firebase target:apply hosting tokenlandia-admin tokenlandia-admin;
+
+firebase deploy --only hosting:tokenlandia-admin;
